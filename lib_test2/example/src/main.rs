26a5afd2b988b9a6b31bf95571062f7c2daaddd7
John Renner <john@jrenner.net>
2018-08-06 11:22:36 -0700

    Add some comments

--- lib_test2/example/src/main.rs
+++ lib_test2/example/src/main.rs
@@ -1,8 +1,13 @@
+// Enable the feature
 #![feature(custom_test_frameworks)]
+
+// Set the test runner
 #![test_runner(extern::test2::test_main_static)]
 
+// Import the macro
 use test2::test2;
 
+// Enjoy
 #[test2]
 #[allow_fail]
 fn my_test() {
